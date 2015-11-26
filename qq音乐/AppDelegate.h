f2ee62d5acf336f80afe66e64cc3171578104a36
mac <mac@macdemacbook-pro.local>
2015-11-26 11:04:11 +0800

    Initial Commit

---
+++ qq音乐/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  qq音乐
+//
+//  Created by mac on 15/11/26.
+//  Copyright © 2015年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ qq音乐Tests/qq__Tests.m
@@ -0,0 +1,39 @@
+//
+//  qq__Tests.m
+//  qq音乐Tests
+//
+//  Created by mac on 15/11/26.
+//  Copyright © 2015年 mac. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface qq__Tests : XCTestCase
+
+@end
+
+@implementation qq__Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
